77ba86ce2d89d6c3f5964355f83b9a3f993eed5c
Julian Valderrama <julian2238rhotmail.com>
2020-10-20 22:04:53 -0500

    agregando angular

---
+++ frontend/src/app/services/employee.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Employee } from '../models/employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  selectedEmployee: Employee;
+
+  constructor(private http: HttpClient) { }
+}
